482d4c089c94c45cf49c0edbf2eb51c7bae89597
Daniel Herrera <daniel.herrera.842@my.csun.edu>
2026-04-28 14:30:55 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- Run Website/server.js
+++ Run Website/server.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
