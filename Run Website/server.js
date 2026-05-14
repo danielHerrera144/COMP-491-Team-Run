7285c137ccdce98b61f541e400a18cf10fefed8c
Daniel Herrera <daniel.herrera.842@my.csun.edu>
2026-05-13 21:24:50 -0700

    Add files via upload

--- Run Website/server.js
+++ Run Website/server.js
@@ -8,7 +8,6 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
